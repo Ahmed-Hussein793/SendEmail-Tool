7ddc4937be07df67f2bd88fe06528c9f79b9713e
Ahmed Hussein <hsynghdban4@gmail.com>
2025-06-22 10:44:17 +0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -129,7 +129,7 @@ document.addEventListener('DOMContentLoaded', function() {
         
         try {
             // Send data to background
-            const response = await fetch('https://sendemail-tool.hsynghdban4.replit.dev/send-email', {
+            const response = await fetch('https://working-dented-weight.glitch.me/send-email', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
